e11671ecb2bf6336c38aef0868b3d3376ffee956
Inzamam Malik <malikasinger1@gmail.com>
2017-02-20 23:22:57 +0500

    not working

---
+++ hw1/ex1.pl
@@ -0,0 +1,7 @@
+thief(john).
+likes(mary,food).
+likes(mary,wine). 
+
+likes(john,X):−likes(X,wine).
+
+may_steal(X,Y):−thief(X),likes(X,Y).
---
+++ hw1/ex2.pl
@@ -0,0 +1,12 @@
+father(X,Y).
+mother(X,Y).
+male(X).
+female(X).
+parent(X,Y).
+diff(X,Y).
+
+ismother(X).
+isfather(X).
+isson(X).
+sisterof(X,Y).
+siblings(X,Y)
